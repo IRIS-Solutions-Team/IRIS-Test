39500115682b4ed35c0089156ea209209ba11244
Jaromir Benes <jaromir.benes@gmail.com>
2017-02-06 20:54:09 +0100

    Clean up

--- run_my_model.m
+++ run_my_model.m
@@ -1,38 +1,41 @@
+% Read and simulate the model
+% Save this file as `run_my_model.m`.
 
-% Read and simulate the model. Save this file as `run_my_model.m`.
-
-% Housekeeping.
+% Clear workspace
 clear;
 close all;
 clc;
+%#ok<*NOPTS>
 
-% Load model file and create a model object.
+% Load model file and create model object
 m = Model('my_simple_rbc.model');
 
-% Assign parameters.
+% Assign parameters
 m.beta = 0.99;
 m.gamma = 0.50;
 m.delta = 0.03;
 m.rho = 0.80;
 m.a = 0.1;
 
-% Find steady state and make sure it really is a valid steady state.
+% Compute steady state and make sure it really is a valid steady state
 m = sstate(m);
 chksstate(m);
 
-% Solve the model.
-m = solve(m);
-disp(m);
+% Compute first-order solution
+m = solve(m) 
 
-% Create an input database for simulation, enter a shock, and display the
-% input time series for the shock on the screen.
-d = zerodb(m, 1:40);
+% Set up input database for simulation
+% * Create a steady-state database
+% * Enter a productivity shock
+% * Display the input time series for the shock on the screen
+d = sstatedb(m, 1:40)
 d.ea(1) = 0.10;
-disp(d.ea{1:5});
+d.ea{1:5}
 
-% Simulate model in shock-minus-control mode.
-s = simulate(m, d, 1:40, 'Deviation=', true, 'AppendPresample=', true);
-disp(s);
+% Simulate model response to the shock
+s = simulate(m, d, 1:40, 'AppendPresample=', true)
 
-% Plot simulated paths.
-dbplot(s, 0:40, {'Y', 'C', 'K', 'A', '400*r'}, 'ZeroLine=', true);
+% Plot simulated paths
+dbplot(s, 0:40, ...
+    {'Y', 'C', 'K', 'A', '400*r'}, ...
+    'ZeroLine=', true);
