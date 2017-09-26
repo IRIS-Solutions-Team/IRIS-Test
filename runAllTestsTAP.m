09887979be8b01fa54bc7573d2b5d628ffea57c1
sergey.plotnikov <sergey.plotnikov@ogresearch.com>
2017-09-26 15:03:45 +0200

    adjusting test runner for TeamCity CI Build: * do not display the tap file on Win machines * remove '#' signs from the beginning of error description lines

--- runAllTestsTAP.m
+++ runAllTestsTAP.m
@@ -22,12 +22,11 @@ try
   % Add the TAPPlugin directed to a file in the Jenkins workspace
   runner.addPlugin(TAPPlugin.producingOriginalFormat(ToFile(thisTAPFile)));
   
+  % run the tests
   runner.run(suite);
   
-  % Display log on Windows machines
-  if ispc
-    disp(fileread(thisTAPFile));
-  end
+  % remove '#' from the error description metadata
+  char2file(regexprep(file2char(thisTAPFile),'\n# ','\n '),thisTAPFile);
   
   % Clean up
   rmpath(thisFolder);
