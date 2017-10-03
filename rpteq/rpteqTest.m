96060a3e49c98585ba2bc4a5b2e9d8f0687e08c4
Jaromir Benes <jaromir.benes@gmail.com>
2017-10-03 15:06:17 +0200

    Fix 15b issues -- final

--- rpteq/rpteqTest.m
+++ rpteq/rpteqTest.m
@@ -33,7 +33,7 @@ outpClip.x = resize(outpClip.x, dates);
 outpClip.z = resize(outpClip.z, dates);
 outpClip.w = resize(outpClip.w, dates);
 
-% Test File 
+%% Test File 
 
 q = rpteq('test.rpteq');
 expectedOutput = outpClip;
@@ -41,7 +41,7 @@ actualOutput = run(q, inp, dates);
 Assert.equal(actualOutput, expectedOutput);
 
 
-% Test File Overlay
+%% Test File Overlay
 
 q = rpteq('test.rpteq');
 expectedOutput = outp;
@@ -49,7 +49,7 @@ actualOutput = run(q, inp, dates, 'dbOverlay=', true);
 Assert.equal(actualOutput, expectedOutput);
 
 
-% Test Model File
+%% Test Model File
 
 m = model('test.model');
 q = get(m, 'reporting');
