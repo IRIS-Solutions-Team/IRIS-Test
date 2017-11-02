a126664e27ba73f5fa45ee4669396c22ddd5fb67
Jaromir Benes <jaromir.benes@gmail.com>
2017-11-02 13:17:12 +0100

    Check if yyaxis is availabe, otherwise exclude tests that call it. Add +visual tests without yyaxis.

--- visual/yyaxis_highlight_line_test.m
+++ visual/yyaxis_highlight_line_test.m
@@ -1,40 +1,84 @@
 
+try
+    yyaxis left
+catch
+    return
+end
+
 close all
 
 f = figure('Visible', 'off');
 ax = gobjects(1, 0);
 for i = 1 : 4
     ax(end+1) = subplot(2, 2, i);
+    yyaxis left
     plot(rand(10)-0.5);
     visual.hline(ax(i), -2);
     visual.highlight(2:4);
-    visual.vline(4, 'Text=', 'This is 4');
+    visual.vline(4);
     visual.zeroline( );
+
+    yyaxis right
+    plot((1:10)-5, 'LineWidth' , 3);
 end
+visual.vline(ax, 6, 'Text', 'This is 6');
+visual.highlight(ax, 6:8);
+visual.zeroline(ax);
+visual.hline(ax, 2);
 
 %% Test Highlight 
 
+try
+    f;
+catch
+    return
+end
+
 h = findobj(f, 'Tag', 'highlight');
-assert(numel(h)==4);
+assert(numel(h)==8);
 
 %% Test VLine 
 
+try
+    f;
+catch
+    return
+end
+
 v = findobj(f, 'Tag', 'vline');
-assert(numel(v)==4);
+assert(numel(v)==8);
 
 %% Test VLine-Caption 
+
+try
+    f;
+catch
+    return
+end
 
 c = findobj(f, 'Tag', 'vline-caption');
 assert(numel(c)==4);
 
 %% Test ZeroLine 
 
+try
+    f;
+catch
+    return
+end
+
 z = findobj(f, 'Tag', 'zeroline');
-assert(numel(z)==4);
+assert(numel(z)==8);
 
 %% Test HLine 
 
+try
+    f;
+catch
+    return
+end
+
 o = findobj(f, 'Tag', 'hline');
-assert(numel(o)==4);
+assert(numel(o)==8);
 
 
