8ec08d4a49bfb5e223919918daf2aa34688fdee9
Jaromir Benes <jaromir.benes@gmail.com>
2017-11-02 12:35:54 +0100

    Add apct test

---
+++ tseries/apct_test.m
@@ -0,0 +1,80 @@
+
+data = exp(cumsum(randn(1000, 4)/10, 1));
+
+%% Test Monthly 
+
+m = tseries(mm(2000, 1), data);
+fm = apct(m);
+actData = fm.Data;
+expData = 100*((data(2:end, :)./data(1:end-1, :)).^12 - 1);
+assert(isequal(actData, expData));
+actStart = fm.Start;
+expStart = mm(2000, 2);
+assert(isequal(actStart, expStart));
+
+%% Test Monthly with Lag 
+
+m = tseries(mm(2000, 1), data);
+fm = apct(m, -6);
+actData = fm.Data;
+expData = 100*((data(7:end, :)./data(1:end-6, :)).^2 - 1);
+assert(isequal(actData, expData));
+actStart = fm.Start;
+expStart = mm(2000, 7);
+assert(isequal(actStart, expStart));
+
+%% Test Monthly with Lead 
+
+m = tseries(mm(2000, 1), data);
+fm = apct(m, +6);
+actData = fm.Data;
+expData = 100*((data(1:end-6, :)./data(7:end, :)).^2 - 1);
+assert(isequal(actData, expData));
+actStart = fm.Start;
+expStart = mm(2000, 1);
+assert(isequal(actStart, expStart));
+
+%% Test Quarterly 
+
+q = tseries(qq(2000, 1), data);
+fq = apct(q);
+actData = fq.Data;
+expData = 100*((data(2:end, :)./data(1:end-1, :)).^4 - 1);
+assert(isequal(actData, expData));
+actStart = fq.Start;
+expStart = qq(2000, 2);
+assert(isequal(actStart, expStart));
+
+%% Test Integer 
+
+i = tseries(10, data);
+fi = apct(i);
+actData = fi.Data;
+expData = 100*((data(2:end, :)./data(1:end-1, :)) - 1);
+assert(isequal(actData, expData));
+actStart = fi.Start;
+expStart = ii(11);
+assert(isequal(actStart, expStart));
+
+%% Test Integer with Lead and Power 
+
+i = tseries(10, data);
+fi = apct(i, +1, 6);
+actData = fi.Data;
+expData = 100*((data(1:end-1, :)./data(2:end, :)).^6 - 1);
+assert(isequal(actData, expData));
+actStart = fi.Start;
+expStart = ii(10);
+assert(isequal(actStart, expStart));
+
+%% Test Daily 
+
+d = tseries(dd(2000, 1, 1), data);
+fd = apct(d);
+actData = fd.Data;
+expData = 100*((data(2:end, :)./data(1:end-1, :)).^365 - 1);
+assert(isequal(actData, expData));
+actStart = fd.Start;
+expStart = dd(2000, 1, 2);
+assert(isequal(actStart, expStart));
+
