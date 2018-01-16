e5893239d9bfcabe87bd8017657434e72bf79c2d
Jaromir Benes <jaromir.benes@gmail.com>
2018-01-16 10:29:23 +0100

    Use new +numeric package function names in existing tseries tests.

--- tseries/unopTest.m
+++ tseries/unopTest.m
@@ -4,26 +4,26 @@ end
 
 
 function testPrctile(this)
-row = 10;
-col = 20;
-pag = 4;
-data = rand(row, col, pag);
-x = tseries(1, data);
+    row = 10;
+    col = 20;
+    pag = 4;
+    data = rand(row, col, pag);
+    x = tseries(1, data);
 
-x1 = prctile(x, [30, 70], 1);
-p1 = tseries.myprctile(data, [30, 70], 1);
-assertEqual(this, x1, p1);
-assertClass(this, x1, 'double');
+    x1 = prctile(x, [30, 70], 1);
+    p1 = numeric.prctile(data, [30, 70], 1);
+    assertEqual(this, x1, p1);
+    assertClass(this, x1, 'double');
 
-x2 = prctile(x, [30, 70], 2);
-p2 = tseries.myprctile(data, [30, 70], 2);
-assertClass(this, x2, 'tseries');
-assertEqual(this, x2.Data, p2);
-assertEqual(this, x2.Comment, repmat({char.empty(1, 0)}, 1, 2, pag));
+    x2 = prctile(x, [30, 70], 2);
+    p2 = numeric.prctile(data, [30, 70], 2);
+    assertClass(this, x2, 'tseries');
+    assertEqual(this, x2.Data, p2);
+    assertEqual(this, x2.Comment, repmat({char.empty(1, 0)}, 1, 2, pag));
 
-x3 = prctile(x, [30, 70], 3);
-p3 = tseries.myprctile(data, [30, 70], 3);
-assertClass(this, x3, 'tseries');
-assertEqual(this, x3.Data, p3);
-assertEqual(this, x3.Comment, repmat({char.empty(1, 0)}, 1, col, 2));
+    x3 = prctile(x, [30, 70], 3);
+    p3 = numeric.prctile(data, [30, 70], 3);
+    assertClass(this, x3, 'tseries');
+    assertEqual(this, x3.Data, p3);
+    assertEqual(this, x3.Comment, repmat({char.empty(1, 0)}, 1, col, 2));
 end
