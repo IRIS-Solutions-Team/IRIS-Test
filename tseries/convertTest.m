4828330196adc1c00a852cfa4235a43b9e06dac3
Jaromir Benes <jaromir.benes@gmail.com>
2018-03-13 17:59:11 +0100

    Add tests for tseries/convert with IgnoreNaN=true

--- tseries/convertTest.m
+++ tseries/convertTest.m
@@ -18,6 +18,32 @@ function testAggregate(this)
     actLast2 = xLast2(:);
     actSelect1 = xSelect1(:);
     actSelect2 = xSelect2(:);
+    assertEqual(this, actMean, [2;5;8;11]);
+    assertEqual(this, actFirst1, [1;4;7;10;13]);
+    assertEqual(this, actFirst2, [1;4;7;10;13]);
+    assertEqual(this, actLast1, [3;6;9;12]);
+    assertEqual(this, actLast2, [3;6;9;12]);
+    assertEqual(this, actSelect1, [1;4;7;10;13]);
+    assertEqual(this, actSelect2, [1.5;4.5;7.5;10.5]);
+end%
+
+
+function testAggregateIgnoreNaN(this)
+    x = Series(mm(2000, 1:13), 1:13);
+    xMean = convert(x, 4, 'Method=', @mean, 'IgnoreNaN=', true);
+    xFirst1 = convert(x, 4, 'Method=', 'first', 'IgnoreNaN=', true);
+    xFirst2 = convert(x, 4, 'Method=', @first, 'IgnoreNaN=', true);
+    xLast1 = convert(x, 4, 'Method=', 'last', 'IgnoreNaN=', true);
+    xLast2 = convert(x, 4, 'Method=', @last, 'IgnoreNaN=', true);
+    xSelect1 = convert(x, 4, 'Select=', 1, 'IgnoreNaN=', true);
+    xSelect2 = convert(x, 4, 'Select=', 1:2, 'IgnoreNaN=', true);
+    actMean = xMean(:);
+    actFirst1 = xFirst1(:);
+    actFirst2 = xFirst2(:);
+    actLast1 = xLast1(:);
+    actLast2 = xLast2(:);
+    actSelect1 = xSelect1(:);
+    actSelect2 = xSelect2(:);
     assertEqual(this, actMean, [2;5;8;11;13]);
     assertEqual(this, actFirst1, [1;4;7;10;13]);
     assertEqual(this, actFirst2, [1;4;7;10;13]);
