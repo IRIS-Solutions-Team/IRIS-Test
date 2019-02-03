2b0acf79804b78bf5ae3107b8e3acd18a3f10915
Jaromir Benes <jaromir.benes@gmail.com>
2019-02-03 22:50:21 +0100

    Add tests of plot spec strings in @tseries and @Series

---
+++ Series/testPlotSpecs.m
@@ -0,0 +1,27 @@
+
+% Setup once
+x = Series(qq(2000), rand(20, 4));
+
+
+%% Test Line Plot with Specs
+
+f = figure('Visible', 'Off');
+plot(x, 'b--');
+close(f);
+
+f = figure('Visible', 'Off');
+h = plot(x, 'b--', 'LineWidth=', 5);
+assert(all([h.LineWidth]==5));
+close(f);
+
+
+%% Test Bar Plot with Specs
+
+f = figure('Visible', 'Off');
+bar(x, 'stacked');
+close(f);
+
+f = figure('Visible', 'Off');
+bar(x, 'grouped');
+close(f);
+
