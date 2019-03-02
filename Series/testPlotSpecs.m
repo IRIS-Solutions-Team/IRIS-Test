7ef702b82f72cc1458c9dc8646a45557c9ffd049
Jaromir Benes <jaromir.benes@gmail.com>
2019-03-02 13:46:32 +0100

    Add plot specs tests separately for letters and nonletters

--- Series/testPlotSpecs.m
+++ Series/testPlotSpecs.m
@@ -3,7 +3,7 @@
 x = Series(qq(2000), rand(20, 4));
 
 
-%% Test Line Plot with Specs
+%% Test Line Plot with Specs Mixing Letters and Nonletters
 
 f = figure('Visible', 'Off');
 plot(x, 'b--');
@@ -11,6 +11,30 @@ close(f);
 
 f = figure('Visible', 'Off');
 h = plot(x, 'b--', 'LineWidth=', 5);
+assert(all([h.LineWidth]==5));
+close(f);
+
+
+%% Test Line Plot with Specs Containing Letters Only
+
+f = figure('Visible', 'Off');
+plot(x, 'b');
+close(f);
+
+f = figure('Visible', 'Off');
+h = plot(x, 'b', 'LineWidth=', 5);
+assert(all([h.LineWidth]==5));
+close(f);
+
+
+%% Test Line Plot with Specs Containing Nonletters Only
+
+f = figure('Visible', 'Off');
+plot(x, '--');
+close(f);
+
+f = figure('Visible', 'Off');
+h = plot(x, 'b', 'LineWidth=', 5);
 assert(all([h.LineWidth]==5));
 close(f);
 
--- legacy.tseries/testPlotSpecs.m
+++ legacy.tseries/testPlotSpecs.m
@@ -2,8 +2,7 @@
 % Setup once
 x = tseries(qq(2000), rand(20, 4));
 
-
-%% Test Line Plot with Specs
+%% Test Line Plot with Specs Mixing Letters and Nonletters
 
 f = figure('Visible', 'Off');
 plot(x, 'b--');
@@ -11,6 +10,30 @@ close(f);
 
 f = figure('Visible', 'Off');
 h = plot(x, 'b--', 'LineWidth=', 5);
+assert(all([h.LineWidth]==5));
+close(f);
+
+
+%% Test Line Plot with Specs Containing Letters Only
+
+f = figure('Visible', 'Off');
+plot(x, 'b');
+close(f);
+
+f = figure('Visible', 'Off');
+h = plot(x, 'b', 'LineWidth=', 5);
+assert(all([h.LineWidth]==5));
+close(f);
+
+
+%% Test Line Plot with Specs Containing Nonletters Only
+
+f = figure('Visible', 'Off');
+plot(x, '--');
+close(f);
+
+f = figure('Visible', 'Off');
+h = plot(x, 'b', 'LineWidth=', 5);
 assert(all([h.LineWidth]==5));
 close(f);
 
