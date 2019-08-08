0e19b098580c0a1c64314ae5cdb9ba551cf26168
Jaromir Benes <jaromir.benes@gmail.com>
2019-08-08 19:34:26 +0200

    Add tests for #211 change steady tolerance

---
+++ Model/toleranceTest.m
@@ -0,0 +1,18 @@
+
+% Set Up Once
+
+testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
+
+
+%% Steady Tolerance Issue #211 
+
+m = Model('toleranceTest.model');
+m = tolerance(m, 'Steady', 1e-1);
+
+tol = tolerance(m, 'Steady');
+
+assertEqual(testCase, tol, 1e-1);
+
+m = steady(m, 'Solver=', {@auto, 'FunctionTolerance=', 1e-1, 'StepTolerance=', 1e-1});
+checkSteady(m);
+
