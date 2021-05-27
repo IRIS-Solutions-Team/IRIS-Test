09b2f9a23493993fd5c6708eb0996d517ef46e77
Jaromir Benes <jaromir.benes@gmail.com>
2021-05-27 16:38:07 +0200

    update

--- Issue-253/issue253Test.m
+++ Issue-253/issue253Test.m
@@ -4,14 +4,14 @@ testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
 
 %% Test True
 
-    m = Model("issue253Test.model", "Assign=", struct("a", 1));
+    m = Model("issue253Test.model", "Assign=", struct('a', 1));
     eq = get(m, 'Equations');
     assertEqual(testCase, string(eq), ["x1=101;"; "x2=102;"]);
 
 
 %% Test False
 
-    m = Model("issue253Test.model", "Assign=", struct("a", 15));
+    m = Model("issue253Test.model", "Assign=", struct('a', 15));
     eq = get(m, 'Equations');
     assertEqual(testCase, string(eq), ["x1=0;"; "x2=0;"]);
 
