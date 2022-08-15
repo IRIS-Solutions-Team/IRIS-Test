a61aae3004cf7bf8809422d66bca3ca947bc5348
Jaromir Benes <jaromir.benes@gmail.com>
2022-08-15 13:37:26 +0200

    Workaround for Matlab bug

--- simple-spbc/simpleSpbcTest.m
+++ simple-spbc/simpleSpbcTest.m
@@ -1,24 +1,62 @@
 
 %% Test all
 
+drawnow();
+
 run01_createModel
+drawnow();
+
 run02_aboutModel
+drawnow();
+
 run03_modifyParameters
+drawnow();
+
 run04_aboutSolution
+drawnow();
+
 run05_playWithGrowthPath
+drawnow();
+
 run06_simulatePlainVanillaShock
+drawnow();
+
 run07_simulateComplexShocks
+drawnow();
+
 run08_simulateDisinflation
+drawnow();
+
 run09_nonlinearSimulation
+drawnow();
+
 run10_resampleFromModel
+drawnow();
+
 run11_prepareDataFromFred
+drawnow();
+
 run12_fisherInfoMatrix
+drawnow();
+
 run13_estimateParams
+drawnow();
+
 run14_morePosterSimulator
+drawnow();
+
 run15_filterHistData
+drawnow();
+
 run16_moreKalmanFilter
+drawnow();
+
 run17_createForecastScenarios
+drawnow();
+
 run18_modelVersusData
+drawnow();
 
 close all
+
 
